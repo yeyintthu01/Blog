af5186060d6ade3bb64987b8cc16ad6f55b5581c
Ye Yint Thu <thuyeyint018@gmail.com>
2023-12-03 16:27:40 +0630

    adding blade layout at blogs

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    <h1><?= $blog->title; ?></h1>
-    <p><?= $blog->body; ?><p>
+@extends('layout')
+
+@section('title')
+    <title>{{$blog->title}}</title>
+@endsection
+@section('content')
+    <h1>{{$blog->title}}</h1>
+    <p>{!!$blog->body!!}<p>
     <a href="/">Go back</a>
-</body>
-</html>
+@endsection
+    
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,19 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    <?php foreach($blogs as $blog) : ?>
-    <h1><a href="blogs/<?= $blog->slug; ?>"><?= $blog->title; ?></a></h1>
+@extends ('layout')
+
+@section('title')
+    <title>All Blogs</title>
+@endsection
+@section('content')
+    @foreach($blogs as $blog)
+        <h1><a href="blogs/{{$blog->slug}}"><?= $blog->title; ?></a></h1>
+        <div>
+            <p>published at - {{$blog->date}}</p>
+            <p>{{$blog->intro}}</p>
+        </div>
+    @endforeach
+@endsection
     
-    <div>
-        <p>published at - <?= $blog->date; ?></p>
-        <p><?= $blog->intro; ?></p>
-    </div>
-    <?php endforeach; ?>
-</body>
-</html>
