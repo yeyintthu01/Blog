99005ea3fa79719fa6535ab65fdb14c59b083bd2
Ye Yint Thu <thuyeyint018@gmail.com>
2023-12-05 00:27:54 +0630

    using fillable and guarded

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -5,7 +5,7 @@
     @foreach($blogs as $blog)
         <h1><a href="blogs/{{$blog->id}}"><?= $blog->title; ?></a></h1>
         <div>
-            <p>published at - {{$blog->date}}</p>
+            <p>published at - {{$blog->created_at->diffForHumans()}}</p>
             <p>{{$blog->intro}}</p>
         </div>
     @endforeach
