b6005319c1b79a87ff814ac38dcd081f26d77e5d
Ye Yint Thu <thuyeyint018@gmail.com>
2023-12-20 23:08:34 +0630

    using queue workers

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Comment;
 use App\Models\Blog;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SubscriberMail;
+
 
 class CommentController extends Controller
 {
@@ -13,12 +17,17 @@ class CommentController extends Controller
         request()->validate([
             'comment'=>'required | min:10'
         ]);
-
         //comment store
         $blog->comments()->create([
             'body' => request('comment'),
             'user_id' => auth()->id()
         ]);
+
+        $subscribers=$blog->subscribers->filter(fn ($subscriber) => $subscriber->id != auth()->id());
+        $subscribers->each(function ($subscriber) use ($blog)
+        {
+            Mail::to($subscriber->email)->queue(new SubscriberMail($blog));
+        });
 
         return redirect('/blogs/'.$blog->slug);
     }
